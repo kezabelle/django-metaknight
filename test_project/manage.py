2027362302ee9cbf6298e9d58b7956260bdf58be
Keryn Knight <keryn@kerynknight.com>
2014-06-01 16:34:41 +0100

    Add request context processor so that treebeard doesnt blow up; see #11 in treebeard github pulls

--- test_project/manage.py
+++ test_project/manage.py
@@ -66,6 +66,17 @@ MOSTLYCACHED_EXCLUDES = (
     r'^.+\.json$',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+)
+
 
 class DoItLazy(object):
     def __iter__(self):
