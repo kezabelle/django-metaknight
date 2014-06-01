d015a751c8653ec518b8891f1767917bf6443b19
Keryn Knight <keryn@kerynknight.com>
2014-06-01 15:08:17 +0100

    beginnings of test project

---
+++ test_project/manage.py
@@ -0,0 +1,61 @@
+from __future__ import unicode_literals
+from __future__ import absolute_import
+import os
+import sys
+from django.conf.urls import url, include
+
+# this module
+me = os.path.splitext(os.path.split(__file__)[1])[0]
+# helper function to locate this dir
+here = lambda x: os.path.join(os.path.abspath(os.path.dirname(__file__)), x)
+
+# SETTINGS
+DEBUG = True
+TEMPLATE_DEBUG = True
+ROOT_URLCONF = me
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': here('db.sqlite3'),
+}}
+TEMPLATE_DIRS = (here('templates'), )
+SECRET_KEY = '$2274f6b78ef89ea2d67e067f498b5a0241c7874c$'
+SITE_ID = 1
+INSTALLED_APPS = (
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.staticfiles",
+    "django.contrib.sitemaps",
+    "django.contrib.messages",
+    "django.contrib.admin",
+    "werkzeug_debugger_runserver",
+    "taggit",
+    "varlet",
+    "editregions",
+)
+STATIC_URL = '/s/'
+MEDIA_URL = '/m/'
+
+
+class DoItLazy(object):
+    def __iter__(self):
+        from django.contrib import admin
+        from varlet import named_urls
+        admin.autodiscover()
+        yield url(r'^admin/', include(admin.site.urls))
+        yield url(r'^', named_urls)
+
+    def __reversed__(self):
+        return reversed(tuple(iter(self)))
+
+
+urlpatterns = DoItLazy()
+
+if __name__ == '__main__':
+    os.environ['DJANGO_SETTINGS_MODULE'] = me
+    sys.path += (here('.'),)
+    # lazy_config()
+    # run the development server
+    from django.core import management
+    management.execute_from_command_line()
