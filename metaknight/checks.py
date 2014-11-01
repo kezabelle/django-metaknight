5eeb787e35dfcbf2db85822dd25a3975321a086a
Keryn Knight <keryn@kerynknight.com>
2014-11-01 13:35:21 +0000

    Add in a system warning if not using django-csp. Warning only, because it's not a simple toggle, mostly.

--- metaknight/checks.py
+++ metaknight/checks.py
@@ -2,6 +2,7 @@
 from django.contrib.admin.checks import ModelAdminChecks
 from django.core.checks import register
 from django.core.checks import Error
+from django.core.checks import Warning
 
 
 @register()
@@ -13,6 +14,19 @@ def adminlinks_installed(app_configs, **kwargs):
             "Cannot use `metaknight` without `adminlinks`",
             hint="put `adminlinks` in your INSTALLED_APPS", obj=None,
             id='metaknight.E1'))
+    return errors
+
+
+@register()
+def csp_polcy(app_configs, **kwargs):
+    errors = []
+    from django.conf import settings
+    if 'csp.middleware.CSPMiddleware' not in settings.MIDDLEWARE_CLASSES:
+        errors.append(Warning(
+            "You don't appear to be using a Content-Security policy",
+            hint="put `csp.middleware.CSPMiddleware` in your "
+                 "MIDDLEWARE_CLASSES, as late as possible", obj=None,
+            id='metaknight.W1'))
     return errors
 
 
